6c847cf9758eb4efed99c7699182d41f758d8d4b
Zolii_ <zolii@hugaming.hu>
2022-09-23 18:21:13 +0200

    Binding fix

--- app/src/main/java/hu/marktmarkt/beadando/MainActivity.java
+++ app/src/main/java/hu/marktmarkt/beadando/MainActivity.java
@@ -1,18 +1,11 @@
 package hu.marktmarkt.beadando;
 
-import static androidx.navigation.Navigation.findNavController;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity<ResultProfileBinding> extends AppCompatActivity {
+    private ResultProfileBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
