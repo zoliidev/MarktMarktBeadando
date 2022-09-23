4d4629d23849f8027d0024b27dbe7d8592fdd852
m-patrik <martipatryk@gmail.com>
2022-09-23 20:09:21 +0200

    Botom Navigation View, hoozzá adva

--- app/src/main/java/hu/marktmarkt/beadando/MainActivity.java
+++ app/src/main/java/hu/marktmarkt/beadando/MainActivity.java
@@ -1,18 +1,46 @@
 package hu.marktmarkt.beadando;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import hu.marktmarkt.beadando.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private FragmentManager frgManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-    }}
+        binding.bottomNavigationView.setOnClickListener(item -> {
+            switch (item.getId()) {
+
+                case R.id.itmMain:
+                    replaceFragment(new MainFragment());
+                    break;
+                case R.id.itmAkcio:
+                    replaceFragment(new AkciókFragment());
+                    break;
+                case R.id.itmProfil:
+                    replaceFragment(new Profil());
+                    break;
+            }
+
+        });
+    }
+    private void replaceFragment(Fragment frg){
+       /* frgManager=new FragmentManager() {
+        };
+        frgManager.findFragmentByTag("frgContainerView");*/
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.lnvNav, frg);
+        fragmentTransaction.commit();
+    }
+}
