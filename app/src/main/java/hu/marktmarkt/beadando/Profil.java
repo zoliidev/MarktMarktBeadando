6cdbfa98b6c681bcd0b27fd44391f9931f0f1f36
Zolii_ <zolii@hugaming.hu>
2022-09-25 15:05:29 +0200

    RestAPI tesztelés

--- app/src/main/java/hu/marktmarkt/beadando/Profil.java
+++ app/src/main/java/hu/marktmarkt/beadando/Profil.java
@@ -4,9 +4,38 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -55,10 +84,51 @@ public class Profil extends Fragment {
         }
     }
 
+    private Button teszt;
+    private EditText prodId;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profil, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profil, container, false);
+
+        teszt = view.findViewById(R.id.button);
+        prodId = view.findViewById(R.id.prodID);
+
+
+        //TODO: ezt kivinni ebből a függvényből
+        teszt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String id = prodId.getText().toString();
+
+                RequestQueue requestQueue = Volley.newRequestQueue(requireContext());
+                String url = "https://oldal.vaganyzoltan.hu/api/product.php";
+
+                StringRequest getProd = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(getContext(), response.toString(), Toast.LENGTH_LONG).show();
+                        //TODO: kapott választ át kell alakítani egy json objektummá, majd ebből hozzunk létre egy új fragmentet.
+                    }
+                }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getContext(), "Hiba történt!", Toast.LENGTH_LONG).show();
+                    }
+                }) {
+                    protected Map<String, String> getParams() {
+                        Map<String, String> MyData = new HashMap<String, String>();
+                        MyData.put("id", prodId.getText().toString());
+                        MyData.put("token", "null");
+                        return MyData;
+                    }
+                };
+                requestQueue.add(getProd);
+            }
+        });
+
+        return view;
     }
 }
