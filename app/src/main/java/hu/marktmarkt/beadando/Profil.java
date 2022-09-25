dfc2a60d16f4534c4f96a72c8293dea237d717a2
Zolii_ <zolii@hugaming.hu>
2022-09-25 15:09:10 +0200

    RestAPI tesztelés

--- app/src/main/java/hu/marktmarkt/beadando/Profil.java
+++ app/src/main/java/hu/marktmarkt/beadando/Profil.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,30 +11,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Cache;
-import com.android.volley.Network;
-import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.BasicNetwork;
-import com.android.volley.toolbox.DiskBasedCache;
-import com.android.volley.toolbox.HttpHeaderParser;
-import com.android.volley.toolbox.HurlStack;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -88,45 +72,30 @@ public class Profil extends Fragment {
     private EditText prodId;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profil, container, false);
-        // Inflate the layout for this fragment
 
         teszt = view.findViewById(R.id.button);
         prodId = view.findViewById(R.id.prodID);
 
 
         //TODO: ezt kivinni ebből a függvényből
-        teszt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String id = prodId.getText().toString();
+        teszt.setOnClickListener(view1 -> {
+            RequestQueue requestQueue = Volley.newRequestQueue(requireContext());
+            String url = "https://oldal.vaganyzoltan.hu/api/product.php";
 
-                RequestQueue requestQueue = Volley.newRequestQueue(requireContext());
-                String url = "https://oldal.vaganyzoltan.hu/api/product.php";
-
-                StringRequest getProd = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Toast.makeText(getContext(), response.toString(), Toast.LENGTH_LONG).show();
-                        //TODO: kapott választ át kell alakítani egy json objektummá, majd ebből hozzunk létre egy új fragmentet.
-                    }
-                }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(getContext(), "Hiba történt!", Toast.LENGTH_LONG).show();
-                    }
-                }) {
-                    protected Map<String, String> getParams() {
-                        Map<String, String> MyData = new HashMap<String, String>();
-                        MyData.put("id", prodId.getText().toString());
-                        MyData.put("token", "null");
-                        return MyData;
-                    }
-                };
-                requestQueue.add(getProd);
-            }
+            StringRequest getProd = new StringRequest(Request.Method.POST, url, response -> {
+                Toast.makeText(getContext(), response, Toast.LENGTH_LONG).show();
+                //TODO: kapott választ át kell alakítani egy json objektummá, majd ebből hozzunk létre egy új fragmentet.
+            }, error -> Toast.makeText(getContext(), "Hiba történt!", Toast.LENGTH_LONG).show()) {
+                protected Map<String, String> getParams() {
+                    Map<String, String> MyData = new HashMap<>();
+                    MyData.put("id", prodId.getText().toString());
+                    MyData.put("token", "null");
+                    return MyData;
+                }
+            };
+            requestQueue.add(getProd);
         });
 
         return view;
