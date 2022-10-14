ca47b12d0e482d59af544f6d4c1563b48e560260
feketekrisa <feketekristof96@gmail.com>
2022-10-14 15:59:53 +0200

    Megrendelés oldalon termékek kilistázva, megrendelés részleg kész. Termék kártya megrendelés listához kész.

--- app/src/main/java/hu/marktmarkt/beadando/orderFragment.java
+++ app/src/main/java/hu/marktmarkt/beadando/orderFragment.java
@@ -15,7 +15,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -34,7 +37,7 @@ import java.util.Map;
 import hu.marktmarkt.beadando.Collection.Util;
 import hu.marktmarkt.beadando.Model.Product;
 
-public class orderFragment extends Fragment {
+public class orderFragment extends Fragment implements AdapterView.OnItemClickListener, AdapterView.OnItemSelectedListener {
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -63,6 +66,22 @@ public class orderFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
+    //Delivery Spinner kiválasztott item
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String courier = parent.getItemAtPosition(position).toString();
+        Toast.makeText(getContext(),courier,Toast.LENGTH_LONG).show();
+    }
+    //Ha semmi nincs kiválasztva
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
 
     class teszt extends RecycleViewAdapter {
         private TextView myTextView;
@@ -80,7 +99,12 @@ public class orderFragment extends Fragment {
 
     private RecyclerView orderRecycleView;
     private NestedScrollView orderNestedScrollView;
+    private Spinner spinner;
+    private TextView orderTextView;
+    private TextView prodCountTextView;
     private ArrayList<Product> cartItem;
+    private Integer prodAmount = 0;
+    private Integer prodTotal = 0;
     RecycleViewAdapter adapter;
 
     @Override
@@ -89,8 +113,28 @@ public class orderFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_order, container, false);
         orderNestedScrollView = view.findViewById(R.id.orderNestedScrollView);
         orderRecycleView = view.findViewById(R.id.orderRecyclerView);
+
+        spinner = view.findViewById(R.id.deliverySpinner);
+        orderTextView = view.findViewById(R.id.orderTextView);
+        prodCountTextView = view.findViewById(R.id.prodCountTextView);
         new Util().removeBars(requireActivity());
 
+        //Spinner
+        String[] arraySpinner = new String[] {
+                "1", "2", "3", "4", "5", "6", "7"
+        };
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.deliveryItems,
+                android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        prodAmount = 0;
+        prodTotal = 0;
+        prodCountTextView.setText("Kosárban: "+prodAmount);
+        orderTextView.setText("Összesen: "+prodTotal+"Ft");
 
         if (cartItem.isEmpty()) {
             loadData();
@@ -131,6 +175,9 @@ public class orderFragment extends Fragment {
             JSONArray Prod = new JSONArray();
             try {
                 Prod = new JSONArray(response);
+                prodAmount += Prod.length();
+                prodCountTextView.setText("Kosárban: "+prodAmount);
+                //Toast.makeText(getContext(),prodAmount.toString(),Toast.LENGTH_LONG).show();
             } catch (JSONException e) {
                 Log.e("GetProduct @ AkciokFragment.java", e.getMessage());
             }
@@ -139,12 +186,14 @@ public class orderFragment extends Fragment {
                 try {
                     String product = Prod.getString(i);
                     String[] splitProd = product.split("@");
+                    prodTotal+= Integer.parseInt(splitProd[2])-((Integer.parseInt(splitProd[2])/100)*Integer.parseInt(splitProd[5]));
+                    //Toast.makeText(getContext(),prodTotal.toString(),Toast.LENGTH_LONG).show();
                     cartItem.add(new Product(Integer.parseInt(splitProd[0]), splitProd[1], Integer.parseInt(splitProd[2]), splitProd[3], splitProd[4], Integer.parseInt(splitProd[5])));
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
-
+            orderTextView.setText("Összesen: "+prodTotal+"Ft");
             showLayout();
 
         }, error -> Toast.makeText(getContext(), error.getMessage() + "", Toast.LENGTH_LONG).show()) {
